39d0a90160c01bf4c286a6c13cf74c319a605d34
h0mygod <eric@donger.ca>
2024-04-20 12:38:37 -0400

    added second page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import './App.css';
 import { TypeAnimation } from 'react-type-animation';
-import React, { useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 function App() {
 
-  const text = () => {
+  /* intersection observer */
+  const obs = new IntersectionObserver((items) => {
+    items.forEach((entry) => {
+        if (entry.rendered === false) {
+            entry.setRendered(true);
+        }
+    })
+  })
+
+  const hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach((el) => obs.observe(el))
+  
+
+  const typingFunc = (text, delay) => {
     return (
       <TypeAnimation
         sequence={[
           ' ',
-          3000,
-          "Hello, nice to meet you."
+          delay,
+          text
         ]}
           wrapper="span"
           cursor={true}
@@ -19,33 +33,47 @@ function App() {
     )
   }
 
-  function DelayedComponent() { 
+  function DelayedComponent(component, delay) { 
     const [rendered, setRendered] = useState(false); 
    
     useEffect(() => { 
       const timer = setTimeout(() => { 
         setRendered(true); 
-      }, 5000); 
+      }, delay); 
    
       return () => clearTimeout(timer); 
-    }, []); 
+    }, [delay]); 
    
     return ( 
       <div> 
-        {rendered && <div className='down-arrow'></div>} 
+        {rendered && component} 
       </div> 
     ); 
   } 
+
+  const downarrow = () => {
+    return (<div className='down-arrow'></div>)
+  }
    
+  const myRef = useRef()
+  
+
   return (
-    <div className="App">
-      <div className='landing-wrapper'>
-        {text()}
+    <div className='App'>
+      <div className="page">
+        <div className='text-wrapper'>
+          {typingFunc("Hello, nice to meet you.", 2500)}
+        </div>
+        <div className='down-arrow-wrapper'>
+          {DelayedComponent(downarrow(), 5000)}
+        </div>
       </div>
-      <div className='down-arrow-wrapper'>
-        {DelayedComponent()}
+      <div className='page'>
+        <div ref={myRef} className='intro-wrapper'>
+        </div>
       </div>
     </div>
+    
   );
 }
 
