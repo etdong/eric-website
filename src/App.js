a11c3ef1baf9c675987ec9a6673e1ff11bf98f00
Eric Dong <eric@donger.ca>
2024-09-08 19:16:45 -0600

    added dividers and pages

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function App() {
   } 
 
   const downarrow = () => {
-    return (<div className='down-arrow'></div>)
+    return (<div href="#page2" className='down-arrow'></div>)
   }
    
   const myRef = useRef()
@@ -61,15 +61,40 @@ function App() {
   return (
     <div className='App'>
       <div className="page">
+      <div class="custom-shape-divider-bottom-1725843514">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+              <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+          </svg>
+      </div>
         <div className='text-wrapper'>
-          {typingFunc("Hello, nice to meet you.", 2500)}
+          {typingFunc("Hello, nice to meet you.", 2000)}
         </div>
         <div className='down-arrow-wrapper'>
           {DelayedComponent(downarrow(), 5000)}
         </div>
       </div>
-      <div className='page'>
+      <div id="page2" className='page'>
+        <div class="custom-shape-divider-top-1725843655">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+          </svg>
+        </div>
         <div ref={myRef} className='intro-wrapper'>
+        </div>
+
+        <div class="custom-shape-divider-bottom-1725844347">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z" class="shape-fill"></path>
+          </svg>
+        </div>
+
+      </div>
+
+      <div id="page3" className='page'>
+        <div class="custom-shape-divider-top-1725844454">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M1200 0L0 0 598.97 114.72 1200 0z" class="shape-fill"></path>
+          </svg>
         </div>
       </div>
     </div>
