b6e4f03375c8dbf0a57ee3fd4c415ab5ac91e91d
etdong <eric@donger.ca>
2025-06-06 12:00:56 +0800

    hashrouter fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Cool from './pages/cool'
 import Home from './pages/home'
 import HomeZh from './pages/home_zh'
 
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route path='/' element={<Home/>}/>
-				<Route path='/zh' element={<HomeZh/>}/>
+		<Routes>
+			<Route path='/' element={<Home/>}/>
+			<Route path='/zh' element={<HomeZh/>}/>
 
-				<Route path='/cool' element={<Cool/>}/>
-			</Routes>
-		</Router>
+			<Route path='/cool' element={<Cool/>}/>
+		</Routes>
 	)
 }
 
