29884c7d347c73fe65c670ea1dd4492c01493195
etdong <eric@donger.ca>
2025-06-06 09:37:25 +0800

    added more info

--- src/pages/home.js
+++ src/pages/home.js
@@ -41,7 +41,7 @@ function Home() {
                     </div>
 
                     <div className='right info'>
-                        <div className='experience'>
+                        <div className='category'>
                             <div className='header'>
                             Experience
                             </div>
@@ -50,14 +50,14 @@ function Home() {
                                 <br />
                                 <i>SINOSEAL</i>
                                 <br />
-                                Project Manager
+                                Project Manager Co-op
                             </div>
                             <div className='subheader'>
                                 Jan 2024 - Aug 2024
                                 <br />
                                 <i>Lumentum</i>
                                 <br />
-                                Software Engineer
+                                Software Engineer Co-op
                             </div>
                             <div className='subheader'>
                                 Sep 2023 - Jan 2024
@@ -67,19 +67,116 @@ function Home() {
                                 Drive Systems Engineer
                             </div>
                         </div>
-                        <div className='skills'>
+                        <div className='category'>
+                            <div className='header'>
+                                Projects
+                            </div>
+                            <div className='subheader'>
+                                <a href='https://github.com/etdong/webhunt'><b>WebHunt</b></a>
+                                <svg fill="#FFFFFF" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 162.656 162.656">
+                                <g>
+                                    <path d="M151.764,10.894c-14.522-14.522-38.152-14.525-52.676-0.008l0.003,0.003L76.112,33.872l10.607,10.605l22.983-22.988
+                                        l-0.002-0.002c8.678-8.663,22.785-8.658,31.457,0.014c8.673,8.672,8.672,22.786,0,31.461l-34.486,34.484
+                                        c-4.201,4.202-9.787,6.516-15.729,6.516c-5.942,0-11.529-2.314-15.73-6.516L64.605,98.052c7.035,7.035,16.389,10.91,26.338,10.91
+                                        c9.949,0,19.303-3.875,26.335-10.91l34.487-34.484C166.284,49.043,166.284,25.413,151.764,10.894z"/>
+                                    <path d="M52.96,141.162L52.96,141.162c-8.675,8.67-22.788,8.668-31.461-0.005c-8.673-8.675-8.673-22.791-0.001-31.465L55.98,75.21
+                                        c8.675-8.674,22.789-8.674,31.462,0L98.05,64.604c-14.524-14.523-38.154-14.524-52.676,0L10.89,99.086
+                                        c-14.519,14.523-14.519,38.154,0.001,52.678c7.263,7.262,16.801,10.893,26.341,10.892c9.536,0,19.074-3.629,26.333-10.887
+                                        l0.002-0.001l22.984-22.99l-10.608-10.606L52.96,141.162z"/>
+                                </g>
+                                </svg>
+                                <br />
+                                Web game based off of GamePigeon's Wordhunt
+                                <br />
+                                <div className='subscript'><i>React, TypeScript, JavaScript, Express.js, Node.js, socket.io, Passport, MongoDB</i></div>
+                            </div>
+                            <div className='subheader'>
+                                <a href='https://github.com/etdong/mitosu'><b>mitosu!</b></a>
+                                <svg fill="#FFFFFF" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 162.656 162.656">
+                                <g>
+                                    <path d="M151.764,10.894c-14.522-14.522-38.152-14.525-52.676-0.008l0.003,0.003L76.112,33.872l10.607,10.605l22.983-22.988
+                                        l-0.002-0.002c8.678-8.663,22.785-8.658,31.457,0.014c8.673,8.672,8.672,22.786,0,31.461l-34.486,34.484
+                                        c-4.201,4.202-9.787,6.516-15.729,6.516c-5.942,0-11.529-2.314-15.73-6.516L64.605,98.052c7.035,7.035,16.389,10.91,26.338,10.91
+                                        c9.949,0,19.303-3.875,26.335-10.91l34.487-34.484C166.284,49.043,166.284,25.413,151.764,10.894z"/>
+                                    <path d="M52.96,141.162L52.96,141.162c-8.675,8.67-22.788,8.668-31.461-0.005c-8.673-8.675-8.673-22.791-0.001-31.465L55.98,75.21
+                                        c8.675-8.674,22.789-8.674,31.462,0L98.05,64.604c-14.524-14.523-38.154-14.524-52.676,0L10.89,99.086
+                                        c-14.519,14.523-14.519,38.154,0.001,52.678c7.263,7.262,16.801,10.893,26.341,10.892c9.536,0,19.074-3.629,26.333-10.887
+                                        l0.002-0.001l22.984-22.99l-10.608-10.606L52.96,141.162z"/>
+                                </g>
+                                </svg>
+                                <br />
+                                AI song recommendation system for the popular rhythm game osu!
+                                <br />
+                                <div className='subscript'>
+                                    <i>Tensorflow, Python, MongoDB, React, JavaScript, Express.js, Node.js</i>
+                                </div>
+                            </div>
+                            <div className='subheader'>
+                                <a href='https://github.com/etdong/ael-astreroids'><b>Asteroids AI</b></a>
+                                <svg fill="#FFFFFF" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 162.656 162.656">
+                                <g>
+                                    <path d="M151.764,10.894c-14.522-14.522-38.152-14.525-52.676-0.008l0.003,0.003L76.112,33.872l10.607,10.605l22.983-22.988
+                                        l-0.002-0.002c8.678-8.663,22.785-8.658,31.457,0.014c8.673,8.672,8.672,22.786,0,31.461l-34.486,34.484
+                                        c-4.201,4.202-9.787,6.516-15.729,6.516c-5.942,0-11.529-2.314-15.73-6.516L64.605,98.052c7.035,7.035,16.389,10.91,26.338,10.91
+                                        c9.949,0,19.303-3.875,26.335-10.91l34.487-34.484C166.284,49.043,166.284,25.413,151.764,10.894z"/>
+                                    <path d="M52.96,141.162L52.96,141.162c-8.675,8.67-22.788,8.668-31.461-0.005c-8.673-8.675-8.673-22.791-0.001-31.465L55.98,75.21
+                                        c8.675-8.674,22.789-8.674,31.462,0L98.05,64.604c-14.524-14.523-38.154-14.524-52.676,0L10.89,99.086
+                                        c-14.519,14.523-14.519,38.154,0.001,52.678c7.263,7.262,16.801,10.893,26.341,10.892c9.536,0,19.074-3.629,26.333-10.887
+                                        l0.002-0.001l22.984-22.99l-10.608-10.606L52.96,141.162z"/>
+                                </g>
+                                </svg>
+                                <br />
+                                Kessler Game fuzzy AI controller
+                                <br />
+                                <div className='subscript'>
+                                    <i>Python, SciKit Learn</i>
+                                </div>
+                            </div>
+                            <div className='subheader'>
+                                <a href='https://github.com/CMPUT301F24throat/throat-events'><b>PickMe Event Lottery App</b></a>
+                                <svg fill="#FFFFFF" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 162.656 162.656">
+                                <g>
+                                    <path d="M151.764,10.894c-14.522-14.522-38.152-14.525-52.676-0.008l0.003,0.003L76.112,33.872l10.607,10.605l22.983-22.988
+                                        l-0.002-0.002c8.678-8.663,22.785-8.658,31.457,0.014c8.673,8.672,8.672,22.786,0,31.461l-34.486,34.484
+                                        c-4.201,4.202-9.787,6.516-15.729,6.516c-5.942,0-11.529-2.314-15.73-6.516L64.605,98.052c7.035,7.035,16.389,10.91,26.338,10.91
+                                        c9.949,0,19.303-3.875,26.335-10.91l34.487-34.484C166.284,49.043,166.284,25.413,151.764,10.894z"/>
+                                    <path d="M52.96,141.162L52.96,141.162c-8.675,8.67-22.788,8.668-31.461-0.005c-8.673-8.675-8.673-22.791-0.001-31.465L55.98,75.21
+                                        c8.675-8.674,22.789-8.674,31.462,0L98.05,64.604c-14.524-14.523-38.154-14.524-52.676,0L10.89,99.086
+                                        c-14.519,14.523-14.519,38.154,0.001,52.678c7.263,7.262,16.801,10.893,26.341,10.892c9.536,0,19.074-3.629,26.333-10.887
+                                        l0.002-0.001l22.984-22.99l-10.608-10.606L52.96,141.162z"/>
+                                </g>
+                                </svg>
+                                <br />
+                                Event scheduling and lottery selection application
+                                <br />
+                                <div className='subscript'>
+                                    <i>Java, Firebase, Espresso, Jest</i>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='category'>
                             <div className='header'>
                                 Skills
                             </div>
                             <div className='subheader'>
                                 Languages:
                                 <br />
-                                Python, C++, Java, JavaScript, React, Node.js, HTML, CSS
+                                Python, Java, C/C++, JavaScript, TypeScript, HTML/CSS, C#, VB.NET, SQL, Rust
                             </div>
                             <div className='subheader'>
-                                Libraries:
+                                Libraries/Frameworks:
                                 <br />
-                                NumPy, Pandas, TensorFlow, PyTorch, Flask, Express.js
+                                React, Node.js, Express.js, TensorFlow, Keras, SciKit Learn, Pandas, NumPy, Matplotlib, JUnit
+                            </div>
+                            <div className='subheader'>
+                                Dev Tools:
+                                <br />
+                                Git, Docker, AWS, Android Studio, MongoDB, Visual Studio, PyCharm, Google Cloud
+                            </div>
+                            <div className='subheader'>
+                                Other:
+                                <br />
+                                REST, Atlassian, Data Structures & Algorithms, Relational Databases, Infrastructure as Code, Agile
                             </div>
                         </div>
                     </div>
